f008aba3f14eab3af4d9ce8e3a38ae70524e2d8f
pupipatsk <pupipat.sk@gmail.com>
2023-05-04 19:51:00 +0700

    finish: Lettuce

---
+++ Exercise/Lab4_2022_2-round2/src/entity/ingredient/Lettuce.java
@@ -0,0 +1,29 @@
+package entity.ingredient;
+
+import entity.base.Choppable;
+import entity.base.Ingredient;
+
+public class Lettuce extends Ingredient 
+	implements Choppable{
+
+	private boolean chopState;
+	
+	public Lettuce() {
+		super("Lettuce");
+		setEdible(true);
+		setChopState(false);
+	}
+	
+	public void chop() {
+		if (!isChopped()) {
+			setChopState(true);
+			setName("Chopped Lettuce");
+		}
+	}
+	public boolean isChopped() {
+		return chopState;
+	}
+	public void setChopState(boolean chopState) {
+		this.chopState = chopState;
+	}
+}
