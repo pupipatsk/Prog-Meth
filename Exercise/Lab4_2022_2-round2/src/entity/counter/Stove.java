fbd42ab63dc0a50522fd0e795b3aa23c33e678f9
pupipatsk <pupipat.sk@gmail.com>
2023-05-04 23:09:54 +0700

    finish: Stove

---
+++ Exercise/Lab4_2022_2-round2/src/entity/counter/Stove.java
@@ -0,0 +1,35 @@
+package entity.counter;
+
+import entity.base.Item;
+import entity.base.Updatable;
+import entity.container.Pan;
+import logic.Player;
+
+public class Stove extends Counter
+	implements Updatable{
+
+	public Stove() {
+		super("Stove");
+	}
+	
+	public Stove(Item content) {
+		super("Stove");
+		setPlacedContent(content);
+	}
+	
+	public void interact(Player p) {
+		if (!this.isPlacedContentEmpty()) {
+			super.interact(p);
+		}
+		else {
+			if (p.getHoldingItem() instanceof Pan) {
+				super.interact(p);
+			}				
+		}
+	}
+	public void update() {
+		if (this.getPlacedContent() instanceof Pan) {
+			((Pan) this.getPlacedContent()).cook();
+		}
+	}
+}
