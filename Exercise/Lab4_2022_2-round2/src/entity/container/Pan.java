f3ae55291997baf6946c4b3faf428b3ab5790a93
pupipatsk <pupipat.sk@gmail.com>
2023-05-04 21:42:02 +0700

    finish: Pan

---
+++ Exercise/Lab4_2022_2-round2/src/entity/container/Pan.java
@@ -0,0 +1,23 @@
+package entity.container;
+
+import entity.base.Container;
+import entity.base.Cookable;
+import entity.base.Ingredient;
+
+public class Pan extends Container{
+
+	public Pan () {
+		super("Pan", 1);
+	}
+	
+	public boolean verifyContent(Ingredient i) {
+		return (i instanceof Cookable);
+	}
+	public void cook() {
+		if(!isEmpty()) {
+			for (Ingredient e : this.getContent()) {
+				((Cookable) e).cook();
+			}
+		}
+	}
+}
