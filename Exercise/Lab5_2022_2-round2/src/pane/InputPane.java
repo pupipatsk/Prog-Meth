df32fc9fca7a92a68c32b6b0d70e524ef2051506
pupipatsk <pupipat.sk@gmail.com>
2023-05-05 14:17:28 +0700

    finish: InputPane

--- Exercise/Lab5_2022_2-round2/src/pane/InputPane.java
+++ Exercise/Lab5_2022_2-round2/src/pane/InputPane.java
@@ -1,16 +1,34 @@
 package pane;
 
-import component.TodoItem;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 
 public class InputPane extends HBox {
     public InputPane() {
         // TODO: FILL CODE
+    	this.setPrefHeight(70);
+    	this.setBackground(Color.LIGHTBLUE);
+    	this.setSpacing(20);
+    	this.setAlignment(Pos.CENTER);
+    	
+    	TextField tf = new TextField();
+    	tf.setPrefWidth(300);
+    	
+    	Button btn = new Button("Submit");
+    	btn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+    		public void handle(MouseEvent e) {
+    			if (!tf.getText().isBlank()) {
+    				TodoItem todo = new TodoItem(tf.getText());
+    				RootPane.getDisplayPane().addTodoItem(todo);
+    				tf.setText(" ");
+    			}
+    		}
+    	});
+    	this.getChildren().addAll(tf, btn);
     }
 }
