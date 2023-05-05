915f0a44481c4fd5d75fe90f4464e65bee261482
pupipatsk <pupipat.sk@gmail.com>
2023-05-05 14:03:32 +0700

    finish: DisplayPane

--- Exercise/Lab5_2022_2-round2/src/pane/DisplayPane.java
+++ Exercise/Lab5_2022_2-round2/src/pane/DisplayPane.java
@@ -1,26 +1,37 @@
 package pane;
 
+import java.util.ArrayList;
+
 import component.TodoItem;
 import javafx.geometry.Insets;
 import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
-import java.util.ArrayList;
-
 public class DisplayPane extends VBox {
     private final ArrayList<ArrayList<TodoItem>> todoLists = new ArrayList<>();
+    
+    // Constructor
     public DisplayPane() {
         // TODO: FILL CODE HERE
+    	this.setBackground(Background.fill(Color.WHITE));
+    	this.setPadding(new Insets(20));
+    	this.setSpacing(10);
     }
 
+    // Methods
     public void addTodoList() {
         // TODO: FILL CODE HERE
+    	ArrayList<TodoItem> todo = new ArrayList<TodoItem>();
+    	todoLists.add(todo);
     }
 
     public void setActiveTodoList(int index) {
         // TODO: FILL CODE HERE
+    	if (0<=index && index<todoLists.size()) {
+    		RootPane.getDisplayPane().getChildren().clear();
+    		RootPane.getDisplayPane().getChildren().addAll(todoLists.get(index));
+    	}
     }
 
     public void addTodoItem(TodoItem todoItem) {
