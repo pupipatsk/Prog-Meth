354a8f835ab0b5b3017b628cc3d80f2efdde8259
pupipatsk <pupipat.sk@gmail.com>
2023-05-05 14:45:46 +0700

    finish: Main

--- Exercise/Lab5_2022_2-round2/src/application/Main.java
+++ Exercise/Lab5_2022_2-round2/src/application/Main.java
@@ -10,6 +10,12 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         // TODO: FILL CODE HERE
+    	RootPane rp = new RootPane();
+    	Scene scene = new Scene(rp, 800, 640);
+    	stage.setScene(scene);
+    	stage.setTitle("SIMPLE(?) TO DO LISTS");
+    	stage.setResizable(false);
+    	stage.show();
     }
 
     public static void main(String[] args) {
