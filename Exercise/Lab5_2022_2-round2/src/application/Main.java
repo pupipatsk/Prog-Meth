dd3ef221aaafb9845929f0d0ce0e72e674e3284e
pupipatsk <pupipat.sk@gmail.com>
2023-05-05 12:37:47 +0700

    Build: Lab5_2022_2-round2

---
+++ Exercise/Lab5_2022_2-round2/src/application/Main.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import pane.RootPane;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        // TODO: FILL CODE HERE
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
