613159ef0b4b49194704ee23557e5a9f4f85b8f4
pupipatsk <pupipat.sk@gmail.com>
2023-05-05 15:57:58 +0700

    finish: ScoreProcessorWrapper

--- Exercise/Lab6_2022_2-round2/src/application/ScoreProcessorWrapper.java
+++ Exercise/Lab6_2022_2-round2/src/application/ScoreProcessorWrapper.java
@@ -1,4 +1,15 @@
 package application;
 
-public class ScoreProcessorWrapper {
+import javafx.scene.control.Label;
+
+public class ScoreProcessorWrapper extends Thread{
+	
+	private ScoreProcessor scoreProcessor;
+	
+	public ScoreProcessorWrapper(BeatMap beatMap, KeyPressTiles keyPressTiles, Label hitScoreLabel, Label missScoreLabel) {
+		scoreProcessor = new ScoreProcessor(beatMap, keyPressTiles, hitScoreLabel, missScoreLabel);
+	}
+	public void run() {
+		scoreProcessor.executeScoreProcessor();
+	}
 }
