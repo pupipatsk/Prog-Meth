48c99f163cb84fadd342fb947be2dc0dbcb4ebb5
pupipatsk <pupipat.sk@gmail.com>
2023-05-05 15:58:13 +0700

    finish: Utility

--- Exercise/Lab6_2022_2-round2/src/application/Utility.java
+++ Exercise/Lab6_2022_2-round2/src/application/Utility.java
@@ -1,22 +1,43 @@
 package application;
 
+import javafx.application.Platform;
 import javafx.scene.control.Label;
 
 public class Utility {
 	public static GameCoreWrapper setUpGameCore(GameCoreWrapper gameCoreWrapper, BeatMap beatMap, NoteTiles noteTiles) {
-		new GameCore(beatMap, noteTiles).executeGameCore();
-		return null;
+		
+		if (gameCoreWrapper != null) {
+			gameCoreWrapper.interrupt();
+		}
+		
+		GameCoreWrapper temp = new GameCoreWrapper( beatMap,  noteTiles);
+		temp.start();
+		return temp;
 	}
 
 	public static ScoreProcessorWrapper setUpScoreProcessor(ScoreProcessorWrapper scoreProcessorWrapper,
 			BeatMap beatMap, KeyPressTiles keyPressTiles, Label hitScoreLabel, Label missScoreLabel) {
-		new ScoreProcessor(beatMap, keyPressTiles, hitScoreLabel, missScoreLabel).executeScoreProcessor();
-		return null;
+		
+		if (scoreProcessorWrapper != null) {
+			scoreProcessorWrapper.interrupt();
+		}
+		
+		ScoreProcessorWrapper temp = new ScoreProcessorWrapper(beatMap, keyPressTiles, hitScoreLabel, missScoreLabel);
+		temp.start();
+		return temp;
 	}
 
 	public static void updateLabel(Label label, String newText) {
+		Platform.runLater( new Runnable() {
+			@Override
+			public void run() {
+				label.setText(newText);
+			}
+		});
 	}
 
 	public static void cleanUp(Menu menu) {
+		menu.getGameCoreWrapper().interrupt();
+		menu.getScoreProcessorWrapper().interrupt();
 	}
 }
