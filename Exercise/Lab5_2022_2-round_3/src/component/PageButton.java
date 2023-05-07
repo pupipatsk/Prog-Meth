430764ca36926a44f71b2fbc69b5ba054a4f8a92
pupipatsk <pupipat.sk@gmail.com>
2023-05-07 16:25:30 +0700

    Lab5_2022_2-round_3: finish: PageButton

---
+++ Exercise/Lab5_2022_2-round_3/src/component/PageButton.java
@@ -0,0 +1,46 @@
+package component;
+
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import pane.RootPane;
+
+public class PageButton extends StackPane{
+	
+	private Circle circle;
+	
+	public PageButton(int pageNumber) {
+		Circle c = new Circle(25);
+		c.setFill(Color.LIGHTGRAY);
+		
+		Text t = new Text(Integer.toString(pageNumber));
+		t.setStyle("-fx-font-size:25;");
+		
+		this.setCircle(c);
+		this.setCursor(Cursor.HAND);
+		this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent event) {
+				RootPane.getNavigationPane().setCurrentPage(pageNumber);
+			}
+		});
+	}
+	
+	public void setActive(boolean value) {
+		if (value == true) {
+			this.getCircle().setFill(Color.WHITE);
+		}
+		else {
+			this.getCircle().setFill(Color.LIGHTGRAY);
+		}
+	}
+	public Circle getCircle() {
+		return circle;
+	}
+	public void setCircle(Circle circle) {
+		this.circle = circle;
+	}
+}
