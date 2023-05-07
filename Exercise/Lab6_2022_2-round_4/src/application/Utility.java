e23a307a85e0dd9646c81c725e89fd107e081189
pupipatsk <pupipat.sk@gmail.com>
2023-05-07 20:43:39 +0700

    Build: Lab6_2022_2-round_4

---
+++ Exercise/Lab6_2022_2-round_4/src/application/Utility.java
@@ -0,0 +1,41 @@
+package application;
+
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+
+public class Utility {
+	public static GameCoreWrapper setUpGameCore(GameCoreWrapper gameCoreWrapper, BeatMap beatMap, NoteTiles noteTiles) {
+		if (gameCoreWrapper != null) {
+			gameCoreWrapper.interrupt();
+//			return null;
+		}
+		GameCoreWrapper temp = new GameCoreWrapper(beatMap, noteTiles);
+		temp.start();
+		return temp;
+	}
+
+	public static ScoreProcessorWrapper setUpScoreProcessor(ScoreProcessorWrapper scoreProcessorWrapper,
+			BeatMap beatMap, KeyPressTiles keyPressTiles, Label hitScoreLabel, Label missScoreLabel) {
+		if (scoreProcessorWrapper != null) {
+			scoreProcessorWrapper.interrupt();
+//			return null;
+		}
+		ScoreProcessorWrapper temp = new ScoreProcessorWrapper(beatMap, keyPressTiles, hitScoreLabel, missScoreLabel);
+		temp.start();
+		return temp;
+	}
+
+	public static void updateLabel(Label label, String newText) {
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				label.setText(newText);				
+			}
+		});
+	}
+
+	public static void cleanUp(Menu menu) {
+		menu.getGameCoreWrapper().interrupt();
+		menu.getScoreProcessorWrapper().interrupt();
+	}
+}
