9fc4b38d5681fe1e9dd1d29fb653347b7eb0fb86
pupipatsk <pupipat.sk@gmail.com>
2023-05-07 20:58:44 +0700

    finish: Lab6_2022_2-round_4

--- Exercise/Lab6_2022_2-round_4/src/application/GameCoreWrapper.java
+++ Exercise/Lab6_2022_2-round_4/src/application/GameCoreWrapper.java
@@ -1,4 +1,14 @@
 package application;
 
-public class GameCoreWrapper {
+public class GameCoreWrapper extends Thread{
+	
+	private GameCore gameCore;
+	
+	public GameCoreWrapper(BeatMap beatMap, NoteTiles noteTiles) {
+		gameCore = new GameCore(beatMap, noteTiles);
+	}
+	
+	public void run(){
+		gameCore.executeGameCore();
+	}
 }
