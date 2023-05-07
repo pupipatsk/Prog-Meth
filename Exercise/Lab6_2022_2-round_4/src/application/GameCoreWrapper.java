2f2ac85e38400f5f83d990178fab7250a8c6db2c
pupipatsk <pupipat.sk@gmail.com>
2023-05-07 20:45:39 +0700

    fix: Lab6_2022_2-round_4

--- Exercise/Lab6_2022_2-round_4/src/application/GameCoreWrapper.java
+++ Exercise/Lab6_2022_2-round_4/src/application/GameCoreWrapper.java
@@ -1,14 +1,4 @@
 package application;
 
-public class GameCoreWrapper extends Thread{
-	
-	private GameCore gameCore;
-	
-	public GameCoreWrapper(BeatMap beatMap, NoteTiles noteTiles){
-		gameCore = new GameCore(beatMap, noteTiles);
-	}
-	
-	public void run() {
-		gameCore.executeGameCore();
-	}
+public class GameCoreWrapper {
 }
--- Exercise/Lab6_2022_2-round_4/src/application/Utility.java
+++ Exercise/Lab6_2022_2-round_4/src/application/Utility.java
@@ -1,41 +1,22 @@
 package application;
 
-import javafx.application.Platform;
 import javafx.scene.control.Label;
 
 public class Utility {
 	public static GameCoreWrapper setUpGameCore(GameCoreWrapper gameCoreWrapper, BeatMap beatMap, NoteTiles noteTiles) {
-		if (gameCoreWrapper != null) {
-			gameCoreWrapper.interrupt();
-//			return null;
-		}
-		GameCoreWrapper temp = new GameCoreWrapper(beatMap, noteTiles);
-		temp.start();
-		return temp;
+		new GameCore(beatMap, noteTiles).executeGameCore();
+		return null;
 	}
 
 	public static ScoreProcessorWrapper setUpScoreProcessor(ScoreProcessorWrapper scoreProcessorWrapper,
 			BeatMap beatMap, KeyPressTiles keyPressTiles, Label hitScoreLabel, Label missScoreLabel) {
-		if (scoreProcessorWrapper != null) {
-			scoreProcessorWrapper.interrupt();
-//			return null;
-		}
-		ScoreProcessorWrapper temp = new ScoreProcessorWrapper(beatMap, keyPressTiles, hitScoreLabel, missScoreLabel);
-		temp.start();
-		return temp;
+		new ScoreProcessor(beatMap, keyPressTiles, hitScoreLabel, missScoreLabel).executeScoreProcessor();
+		return null;
 	}
 
 	public static void updateLabel(Label label, String newText) {
-		Platform.runLater(new Runnable() {
-			@Override
-			public void run() {
-				label.setText(newText);				
-			}
-		});
 	}
 
 	public static void cleanUp(Menu menu) {
-		menu.getGameCoreWrapper().interrupt();
-		menu.getScoreProcessorWrapper().interrupt();
 	}
 }
